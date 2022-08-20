c931c15963d6addbd5155ff2bba637e9cc26865b
Vladimir <70mr.mystory@gmail.com>
2022-08-20 22:02:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const storeName = "Decathlon";
+
+const storeDescription = {
+   budget: 10000,
+   employees: ["Mark", "Jhon", "Anna"],
+   products: {
+      snickers: 260,
+      ball: 25
+   },
+   open: true,
+};
+
+
+
+
